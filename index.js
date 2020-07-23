d3daa5fa4c37be9a29c73a546945784451bf0e0d
bsk-123 <43913935+bsk-123@users.noreply.github.com>
2020-07-23 17:06:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 document.querySelector(".new").addEventListener("click",function(){
+    console.log(screen.width)
+    if(screen.width<768){
+        console.log(screen.width)
+    document.querySelector(".right").style.display="unset"
+    document.querySelector(".forms").style.display="unset"
+    }else{
+    document.querySelector(".right").style.display="flex"
+    document.querySelector(".forms").style.display="flex"
+    }
     document.querySelector(".signin").style.display="none"
     document.querySelector(".or").style.display="none"
     document.querySelector(".logos").style.display="none"
     document.querySelector(".n").style.display="none"
-    document.querySelector(".register").style.display="inline-block"
-    document.querySelector(".l").style.display="inline-block"
+    document.querySelector(".register").style.display="block"
+    document.querySelector(".l").style.display="block"
     document.querySelector(".head").innerHTML="Welcome To Yaar Me, Register Here!"
     document.querySelector(".submit").setAttribute("value","Register")
 })
 document.querySelector(".ll").addEventListener("click",function(){
+    document.querySelector(".right").style.display="flex"
+    document.querySelector(".forms").style.display="flex"
     document.querySelector(".signin").style.display="inline-block"
     document.querySelector(".or").style.display="flex"
     document.querySelector(".logos").style.display="flex"
